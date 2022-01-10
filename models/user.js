d31a93f2e824f5f197649a98b9b666ed147581df
Riaz <riazzzz@gmail.com>
2022-01-10 13:47:41 -0800

    user login seems to be working

--- models/user.js
+++ models/user.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    googleId: String // <-- This property needs to be on your user model for your project
+  
+  }, {
+    timestamps: true
+  });
+
+  module.exports = mongoose.model('User', userSchema);
+
 // Create your User Model
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const usersCtrl = require('../controllers/students');
+const usersCtrl = require('../controllers/users');
 
 // GET /students
 router.get('/users', usersCtrl.index);
@@ -8,12 +8,12 @@ router.get('/users', usersCtrl.index);
 // Authorizing the user to use a route
 // probably only want to use this on
 // post, put or delete routes
-// function isLoggedIn(req, res, next) {
-// 	// req.isAuthenticated() this is given to us by passport
-// 	// it returns true or false
-// 	if ( req.isAuthenticated() ) return next(); // next() go to the next function in middleware, above situation studentsCtrl.addFact
-// 	res.redirect('/auth/google'); // redirect them to login
-// }
+function isLoggedIn(req, res, next) {
+	// req.isAuthenticated() this is given to us by passport
+	// it returns true or false
+	if ( req.isAuthenticated() ) return next(); // next() go to the next function in middleware, above situation studentsCtrl.addFact
+	res.redirect('/auth/google'); // redirect them to login
+}
 
 
 
