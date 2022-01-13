40857795d1f0189765abfb48ca95b3ea01cdee36
Riaz <riazzzz@gmail.com>
2022-01-13 11:27:19 -0800

    Deploy to Heroku

--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,7 @@ const bookCtrl = require('../controllers/books');
 router.get('/', bookCtrl.index);
 router.get('/new', bookCtrl.new);
 router.post('/', bookCtrl.create);
-router.get('/:id', bookCtrl.show)
+router.get('/:id', bookCtrl.show);
+// router.get('/advancedsearch', bookCtrl.advancedSearch)
 
 module.exports = router;
